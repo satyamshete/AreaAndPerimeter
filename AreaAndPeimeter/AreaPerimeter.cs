bb6a76e953dc75333ff5def0d6e0c3ff45fba700
satyamshete <shetesatyam99@gmail.com>
2022-08-12 23:05:17 +0530

    [Satyam]: Add Review Area And Perimeter of Rectangle and Circle

---
+++ AreaAndPeimeter/AreaPerimeter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndPeimeter
+{
+    internal class AreaPerimeter
+    {
+        internal void AreaAndPerimeterOfRectangle(int length, int breadth)
+        {
+            int area;
+            int perimeter;
+            area = length * breadth;
+            perimeter = 2 * (length + breadth);
+            Console.WriteLine("Area and perimeter of rectangle with length: {0} and breadth: {1} is {2} and {3} respectively ", length, breadth, area, perimeter);
+        }
+
+        internal void AreaAndPerimeterOfCircle(int radius)
+        {
+
+            const float pi = 3.1416f;
+            float perimeter = 2 * pi * radius;
+            float area = pi * radius * radius;
+          
+            Console.WriteLine("Area and perimeter of cirvle with radius: {0} is {1} and {2} respectively ", radius, area, perimeter);
+
+        }
+    }
+}
---
+++ AreaAndPeimeter/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AreaAndPeimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area And Perimeter of Rectangle and Circle");
+            Console.WriteLine("Area and perimeter finder");
+            Console.WriteLine("Press 1 for");
+            Console.WriteLine("Area and perimeter of reactangle");
+            Console.WriteLine("Press 2 for");
+            Console.WriteLine("Area and perimeter of circle");
+            
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Enter length");
+                    int length = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter breadth");
+                    int breadth = Convert.ToInt32(Console.ReadLine());
+                    AreaPerimeter reactangle = new AreaPerimeter();
+                    reactangle.AreaAndPerimeterOfRectangle(length, breadth);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Radius");
+                    int radius = Convert.ToInt32(Console.ReadLine());
+                    AreaPerimeter circle = new AreaPerimeter();
+                    circle.AreaAndPerimeterOfCircle(radius);
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choise");
+                    break;
+            }
+        }
+    }
+    
+}
